b80cbaa75d8bc02e7d519f85759254648379dc68
Cynthia Vocal <vocal.cynthia@gmail.com>
2024-06-14 00:48:43 -0300

    solucionV1

---
+++ src/main/java/com/api/clientes/ClientesApplication.java
@@ -0,0 +1,16 @@
+package com.api.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class ClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesApplication.class, args);
+	}
+
+}
